439096693ca87003fd0e5ccac52539d46003533a
ashachakre0906 <chourpagar.asha@gmail.com>
2022-07-16 10:28:19 -0700

    pushing my-sql-todo

---
+++ db/connection.js
@@ -0,0 +1,10 @@
+require ('dotenv').config();
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,//environment variable
+    database: 'todos_db',
+}).promise();
+
+module.exports = connection;
---
+++ model/seeds.sql
@@ -0,0 +1,7 @@
+USE todos_db;
+INSERT INTO todos (TODO, isCompleted)
+VALUES( 'run',true),
+('fold clothes',true),
+('Walking',true),
+('Swimming',false);
+
